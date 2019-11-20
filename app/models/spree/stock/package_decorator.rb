afbaec669b7d914076c4e4a3a72280b1356ae540
johndavid400 <johndavid400@gmail.com>
2019-11-20 17:12:31 -0600

    fix vendor shipping methods not adhering to shipping_category

--- app/models/spree/stock/package_decorator.rb
+++ app/models/spree/stock/package_decorator.rb
@@ -1,7 +1,8 @@
 Spree::Stock::Package.class_eval do
   def shipping_methods
     if (vendor = stock_location.vendor)
-      vendor.available_shipping_methods.to_a
+      #vendor.available_shipping_methods.to_a
+      shipping_categories.map(&:shipping_methods).reduce(:&).to_a & vendor.shipping_methods.to_a
     else
       shipping_categories.map(&:shipping_methods).reduce(:&).to_a
     end
