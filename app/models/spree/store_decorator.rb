2bdde4e960ae300307a177381c7239bcbcc0cb8b
johndavid400 <johndavid400@gmail.com>
2020-05-07 16:29:23 -0500

    vendor-stores

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -1,5 +1,10 @@
 module Spree
   Store.class_eval do
     has_and_belongs_to_many :vendors, join_table: 'spree_stores_vendors'
+
+    def products
+      vendors.products
+    end
+
   end
 end
--- app/models/spree/store_vendor.rb
+++
@@ -1,2 +0,0 @@
-class Spree::StoreVendor < ApplicationRecord
-end
