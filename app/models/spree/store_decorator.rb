e2505341c642f0669a9b7b3b7a650f922a6922b6
johndavid400 <johndavid400@gmail.com>
2020-06-11 17:17:13 -0500

    if default store and no vendors, show all products

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -3,7 +3,11 @@ module Spree
     has_and_belongs_to_many :vendors, join_table: 'spree_stores_vendors'
 
     def products
-      vendors.products
+      if default?
+        vendors.any? ? vendors.products : Spree::Product.all
+      else
+        vendors.products
+      end
     end
 
   end
